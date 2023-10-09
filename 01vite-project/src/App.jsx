5d758a71b52b159e5f7a4838f7dbd3d22f991c79
AnujSingh001 <anujbudhwar88@gmail.com>
2023-10-09 15:03:07 +0530

    start ract learning

---
+++ 01vite-project/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import viteLogo from '/vite.svg'
+import Chai from './chai';
+
+function App() {
+  return (
+    <>
+    <Chai/>
+    <h1>Hello from chai</h1>
+    <p>its a test case so dont worry about it</p>
+    </>
+    
+    
+
+  ); 
+}
+
+export default App
