c453c6aaa3081e7fefccb4b75e2f41a240754b7f
AnujSingh001 <anujbudhwar88@gmail.com>
2023-10-14 17:36:42 +0530

    theme Switcher project

---
+++ 09themeSwitcher/src/context/theme.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { createContext, useContext } from "react";
+
+export const ThemeContext = createContext({
+    themeMode: 'light',
+    darktheme : ()=>{},
+    lighttheme : ()=>{}
+})
+
+export const ThemeProvider = ThemeContext.Provider
+
+export default function useTheme(){
+    return useContext(ThemeContext)
+}
+
