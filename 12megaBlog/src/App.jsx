503357cc3d48e0811def4c2668b144244a4445bc
AnujSingh001 <anujbudhwar88@gmail.com>
2023-10-22 22:39:59 +0530

    project

--- 12megaBlog/src/App.jsx
+++ 12megaBlog/src/App.jsx
@@ -1,14 +1,29 @@
-
+import {React, useState, useEffect, useDispatch } from 'react'
 import './App.css'
+import authService from './appWrite/auth'
+import  Header  from './component/Header/Header'
+import Footer from './component/Footer/Footer'
+import { Outlet } from 'react-router-dom'
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITER_URL)
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch
 
-  return (
-    <>
-     <h1>Blog with Appwfrite</h1>
-    </>
-  )
+  useEffect(()=>{
+
+  },[])
+
+
+  return loading ? (
+  <div className='bg-gray-600 flex flex-wrap content-between min-h-sh'>
+    <div className='w-full block'>
+      <Header/>
+      <main>
+        Todo:{/* <Outlet/> */}
+      </main>
+      <Footer/>
+    </div>
+  </div>):null
 }
 
 export default App
